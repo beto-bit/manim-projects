576b902aa1c0c4c1b46f928f651181688ac4a194
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-02-07 12:48:29 -0600

    Added enclosing_rectangle function
    Also changed background color, some type annotations, **kwargs passing and changed the example.

--- scene.py
+++ scene.py
@@ -1,21 +1,25 @@
 from manim import *
 
+config.background_color = "#ece6e2"
+
 class Trapezoid(Polygon):
     def __init__(
         self,
-        a: int,
-        b: int,
+        a: float,
+        b: float,
         center=False,
         **kwargs
     ):
         self.a_length = a
         self.b_length = b
+        self.base_length = a + b
 
         super().__init__(
             [a + b, b, 0],  # Top right
             [0, a, 0],      # Top left
             [0, 0, 0],      # Bottom left
             [a + b, 0, 0],  # Bottom right
+            **kwargs
         )
 
         if center:
@@ -38,23 +42,32 @@ class Trapezoid(Polygon):
         return self.get_vertices()[3]
 
 
-    def fitting_triangle(self) -> Polygon:
+    def fitting_triangle(self, **kwargs) -> Polygon:
         middle = self.bottom_left + [self.b_length, 0, 0]
 
         return Polygon(
             self.top_right,
             self.top_left,
-            middle
+            middle,
+            **kwargs
         )
+
+    def enclosing_rectangle(self, **kwargs) -> Rectangle:
+        return Rectangle(
+            width=self.base_length,
+            height=max(self.a_length, self.b_length),
+            **kwargs
+        ).move_to(self)
 
 
 class Test(Scene):
     def construct(self):
-        trapezoid = Trapezoid(2, 6, center=True)
-        triangle = trapezoid.fitting_triangle()
+        trapezoid = Trapezoid(3.3, 6, center=True, color=BLACK)
+        triangle = trapezoid.fitting_triangle(color=BLACK)
+        rectangle = trapezoid.enclosing_rectangle(color=BLACK)
 
-        self.play(Write(trapezoid))
-        self.play(Write(triangle))
+        figure = VGroup(trapezoid, triangle, rectangle)
+
+        self.play(Write(figure))
+
         self.wait(1)
-
-        pass
