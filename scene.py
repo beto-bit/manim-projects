a84df3d0fa624b1c1e8cafd37059ae2fe5f9debf
beto-bit <76757367+beto-bit@users.noreply.github.com>
2023-02-07 11:56:33 -0600

    Moved Trapezoid to it's own class.

--- scene.py
+++ scene.py
@@ -1,22 +1,31 @@
 from manim import *
 
-def create_trapezoid(a: int, b: int, center=False) -> Polygon:
-    trapezoid = Polygon(
-        [a + b, b, 0],  # Top right
-        [0, a, 0],      # Top left
-        [0, 0, 0],      # Bottom left
-        [a + b, 0, 0],  # Bottom right 
-    )
+class Trapezoid(Polygon):
+    def __init__(
+        self,
+        a: int,
+        b: int,
+        center=False,
+        **kwargs
+    ):
+        self.a_length = a
+        self.b_length = b
 
-    if not center:
-        return trapezoid
+        super().__init__(
+            [a + b, b, 0],  # Top right
+            [0, a, 0],      # Top left
+            [0, 0, 0],      # Bottom left
+            [a + b, 0, 0],  # Bottom right
+        )
 
-    return trapezoid.shift(DOWN * b / 2 + LEFT * (a + b) / 2)
+        if center:
+            self.shift(DOWN * b / 2 + LEFT * (a + b) / 2)
 
 
 class Test(Scene):
     def construct(self):
-        trapezoid = create_trapezoid(2, 6, center=True)
+        trapezoid = Trapezoid(2, 6, center=True)
+
 
         self.play(Write(trapezoid))
         self.wait(1)
